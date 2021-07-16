2b1123bc15628994c1d8ca0a117aeb67569b7960
Luiz Arthur <luizarthurvm@gmail.com>
2021-07-16 09:39:59 -0300

    Acessando API do github

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,7 +1,7 @@
 import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
-    const tron = Reactotron.configure({ host: '192.168.11.9' })
+    const tron = Reactotron.configure({ host: '192.168.11.11' })
         .useReactNative()
         .connect();
 
